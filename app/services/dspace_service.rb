17ba11cd7b87fb425143fde81fdf9ae91ea34ecd
Bruno Nocera Zanette <bnzanette@inf.ufpr.br>
2016-09-06 13:11:03 -0300

    fix Dspace's production parameters

--- app/services/dspace_service.rb
+++ app/services/dspace_service.rb
@@ -4,7 +4,7 @@ class DspaceService < Dspace::Client
 
   def self.create_client
     dspace_client = self.new(dspace_api: config['link'])
-    dspace_client.login config['login'], config['password']
+    # dspace_client.login config['login'], config['password'] ? puts "NO_LOGIN"
     dspace_client
   end
 
@@ -17,18 +17,12 @@ class DspaceService < Dspace::Client
   def self.config
     config = {}
 
-    if Rails.env.production?
-      config['link'] = "https://#{ENV['PORTALMEC_DSPACE_HOST']}:#{ENV['PORTALMEC_DSPACE_PORT']}"
-      config['login'] = ENV['PORTALMEC_DSPACE_LOGIN']
-      config['password'] = ENV['PORTALMEC_DSPACE_PASSWORD']
-    else
-      yml_file = YAML.load_file(Rails.root.join('config').to_s.concat('/dspace.yml'))
-      yml = yml_file.fetch(Rails.env)
+    yml_file = YAML.load_file(Rails.root.join('config').to_s.concat('/dspace.yml'))
+    yml = yml_file.fetch(Rails.env)
 
-      config['link'] = "https://#{yml['host']}:#{yml['port']}"
-      config['login'] = yml['login']
-      config['password'] = yml['password']
-    end
+    config['link'] = "https://#{yml['host']}:#{yml['port']}"
+    config['login'] = yml['login']
+    config['password'] = yml['password']
 
     config
   end
