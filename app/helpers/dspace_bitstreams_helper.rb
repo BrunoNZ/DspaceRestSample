5087b63464d94777de03048c0a73daf70612b4f5
Bruno Nocera Zanette <bnzanette@inf.ufpr.br>
2016-09-13 22:55:17 -0300

    fix bug: Wrong download link to Bitstreams

--- app/helpers/dspace_bitstreams_helper.rb
+++ app/helpers/dspace_bitstreams_helper.rb
@@ -1,5 +1,5 @@
 module DspaceBitstreamsHelper
   def retrieve_link_of bitstream
-    DspaceService.link + '/rest' + bitstream.retrieve_link
+    DspaceService.link + bitstream.retrieve_link
   end
 end
