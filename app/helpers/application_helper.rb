d08c708ae360e3d737ecf567c9fb9d24b650b907
Bruno Nocera Zanette <bnzanette@inf.ufpr.br>
2016-09-20 19:29:07 -0300

    improve PageSelection style (use Bootstrap)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def previsous_page_status page
+    page == 0 ? 'disabled' : 'enabled'
+  end
+
+  def next_page_status objects
+    (objects.empty? || objects.size < 25) ? 'disabled' : 'enabled'
+  end
+
+  def min_numbered_page page
+    page-2 < 0 ? 0 : page-2
+  end
+
+  def max_numbered_page(page,objects)
+    (objects.empty? || objects.size < 25) ? page : page+2
+  end
 end
