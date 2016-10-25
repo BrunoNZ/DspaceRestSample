3b6b323bccfb50257be0419f0357e6c0e7f8bfac
Bruno Nocera Zanette <bnzanette@inf.ufpr.br>
2016-10-25 12:26:38 -0200

    reorganize DspaceService

--- app/services/dspace_service.rb
+++ app/services/dspace_service.rb
@@ -2,18 +2,27 @@ require 'dspace'
 
 class DspaceService < Dspace::Client
 
+  def self.link
+    @link ||= config['link']
+  end
+
+  def self.bitstreams_path
+    @bitstreams_path ||= config['bitstreams_path']
+  end
+
   def self.client
-    @link ||= link
-    @dspace_client ||= DspaceService.new(dspace_api: @link)
-    @dspace_client
+    @dspace_client ||= DspaceService.new(dspace_api: link)
   end
 
   private
 
-  def self.link
+  def self.config
     yml_file = YAML.load_file(Rails.root.join('config').to_s.concat('/dspace.yml'))
     yml = yml_file.fetch(Rails.env)
-    link = "https://#{yml['host']}:#{yml['port']}"
-    link
+    config = Hash.new
+    config['link'] = "https://#{yml['host']}:#{yml['port']}"
+    config['bitstreams_path'] = yml['bitstreams_path']
+    config
   end
+
 end
